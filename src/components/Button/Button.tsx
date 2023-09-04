296ff846aac4e0016763fa748b5eb1dfabadee09
rodrigokovalski <rodrigokvk@gmail.com>
2023-09-03 22:32:11 -0300

    fix: linting button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { StyledButton } from './Button.styles';
 import { ButtonProps } from './Button.types';
 
-const Button: React.FC<ButtonProps> = (props) => <StyledButton {...props} onClick={props.onClick}>{props.children}</StyledButton>
+const Button: React.FC<ButtonProps> = (props) => (
+    <StyledButton {...props} onClick={props.onClick}>
+        {props.children}
+    </StyledButton>
+);
 
-export default Button
+export default Button;
