76bd98b478bd7e07bf5f1fb9df8e5ab821deb6df
rodrigokovalski <rodrigokvk@gmail.com>
2023-09-06 01:12:23 -0300

    Subindo ajustes

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import terser from "@rollup/plugin-terser";
 
 import { readFileSync } from "fs";
 const packageJson = JSON.parse(readFileSync('package.json', {encoding: 'utf8'}));
@@ -27,7 +26,6 @@ export default [
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
-            terser()
         ]
     },
     {
